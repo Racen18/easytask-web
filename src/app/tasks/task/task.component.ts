c33eb9fbb1aeeffd6a4e6e96b60c788cfe23d536
Thamilarasan M <thamilarasan.m@tesark.in>
2025-02-27 19:11:45 +0530

    feat: complex module based structure

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,7 +1,5 @@
 import { Component, inject, Input } from '@angular/core';
 import { type Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
 
 @Component({
